96add95a3043c58c1c146096ab398a7e2ba32ca2
Katrien S <ontwerp@katriens.be>
2023-06-22 16:16:34 +0200

    Challenge 2 (unfunished) + Challenge 33

---
+++ 002-big-menu/app.js
@@ -0,0 +1,7 @@
+const chevronArrowUp = document.querySelectorAll('.fa-chevron-up');
+const chevronArrowDown = document.querySelectorAll('.fa-chevron-down');
+
+console.log(chevronArrowDown);
+console.log(chevronArrowUp);
+
+
