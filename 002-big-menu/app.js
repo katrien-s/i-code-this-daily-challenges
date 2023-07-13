c5040ccdb2cdf4f9c38e2b5d8612af2d69e1c99e
Katrien S <ontwerp@katriens.be>
2023-07-13 14:53:16 +0200

    Finished CCS

--- 002-big-menu/app.js
+++ 002-big-menu/app.js
@@ -1,7 +1,1 @@
-const chevronArrowUp = document.querySelectorAll('.fa-chevron-up');
-const chevronArrowDown = document.querySelectorAll('.fa-chevron-down');
-
-console.log(chevronArrowDown);
-console.log(chevronArrowUp);
-
 
