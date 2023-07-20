7491d0a52c3f7f539dc1e24b2ce5a11f5ae451d6
Katrien S <ontwerp@katriens.be>
2023-07-20 17:30:10 +0200

    Challenge 44 - html structure

---
+++ 044-music-store/data.js
@@ -0,0 +1,127 @@
+const albums = [
+	{
+		artist: 'African Head Charge',
+		title: 'A Trip to Bolgatanga',
+		'release-date': 2023,
+		'record-label': 'On-U-Sound',
+		'wholesale price': 15,
+		description:
+			'Where previous albums have been a melting pot of global influences, on their new LP African Head Charge have but one place in mind. »A Trip To Bolgatanga« is a musical journey to Bonjo’s current hometown in north Ghana. A psychedelic travelogue across the landscape featuring their trademark hand percussion and group chanting augmented with rumbling bass, mutated horns, dubbed out effects, wild wah-wah, haunted voodoo dancehall, synthetic swells, disco congas, tumbling layers of electronic effects, blues-inflected woodwind, and funky organ.',
+		rating: 4,
+		// url:,
+		explore: true,
+		'top-sales': false,
+		recommended: true
+	},
+	{
+		artist: 'Carmen Villain',
+		title: 'Music From The Living Monument',
+		'release-date': 2023,
+		'record-label': 'Smalltown Supersound',
+		'wholesale price': 15,
+		description:
+			"Each tableau is bound together by Carmen Villain’s hypnotic score in which the audience is taken on a meditative journey through vibrant tableaus in a dreamlike universe. Carmen Villain's score is a suspension of time where her music is seeking a new form of slow-moving minimalism.",
+		rating: 4,
+		// url: ,
+		explore: true,
+		'top-sales': false,
+		recommended: true
+	},
+	{
+		artist: 'Aphex Twin',
+		title: 'Blackbox Life Recorder 21f / in a room7 F760',
+		'release-date': '28-07-2023',
+		'record-label': 'Warp',
+		'wholesale price': 18,
+		description:
+			'First AFX material in five years. The machines co-opted in production are as complex as the Weirdcore artwork, layering signature sombre ambient chords with intricate drum programming.',
+		rating: 4,
+		// url: ,
+		explore: false,
+		'top-sales': true,
+		recommended: false
+	},
+	{
+		artist: 'Matana Roberts',
+		title: 'Coin Coin Chapter Five: In The Garden...',
+		'release-date': '11-09-2023',
+		'record-label': 'Constellation',
+		'wholesale price': 18,
+		description:
+			'Avant folk-jazz-blues visionary Matana Roberts braids African-American history and music in a remarkable 5th chapter to her epic, ongoing 12-part cycle - co-produced by TV on the Radio’s Kyp Malone, overseen by the spirit of Jaimie Branch (RIP), and performed by her 11-piece ensemble.',
+		rating: 4,
+		explore: false,
+		'top-sales': true,
+		recommended: false
+		// url: ,
+	},
+	{
+		artist: 'Loraine James',
+		title: 'Gentle Confrontation',
+		'release-date': '22-09-2023',
+		'record-label': 'Hyperdub',
+		'wholesale price': 16,
+		description:
+			'Loraine James lets her influences run wild on her third Hyperdub volume, sampling DNTEL, Telefon Tel Aviv and Lusine on tracks that dig into her emotional core. Featuring guest appearances from Marina Herlop, keiyaA, Corey Mastrangelo, Eden Samara, George Riley and Contour.',
+		rating: 4,
+		// url: ,
+		explore: false,
+		'top-sales': true,
+		recommended: false
+	},
+	{
+		artist: 'Laurel Halo',
+		title: 'Atlas',
+		'release-date': '22-09-2023',
+		'record-label': 'Awe',
+		'wholesale price': 15,
+		description:
+			"Laurel Halo's long-in-the-making debut album for her newly minted Awe label is dazzling; a mix of weightless jazz, orchestral and drift energies that’s both elusive and engrossing; just when you think you have the measure of it, it shapeshifts into something else. Made of rarified material; it bends the contemporary “ambient” template into something almost entirely new, creating a blanket of pure atmosphere that wafts over you like a cloud, but which fully comes to life with closer, deep listening.",
+		rating: 4,
+		// url: ,
+		explore: false,
+		'top-sales': false,
+		recommended: true
+	},
+	{
+		artist: 'Yann Novak',
+		title: 'The Voice of Theseus',
+		'release-date': '07-07-2023',
+		'record-label': 'Room 40',
+		'wholesale price': 15,
+		description: 'The Voice of Theseus by Yann Novak, via Room 40.',
+		rating: 4,
+		// url: ,
+		explore: false,
+		'top-sales': false,
+		recommended: true
+	},
+	{
+		artist: 'Ana Roxanne & DJ Python',
+		title: 'Natural Wonder Beauty Concept',
+		'release-date': '14-07-2023',
+		'record-label': 'Mexican Summer',
+		'wholesale price': 15,
+		description:
+			'Tender mutuals, ambient starlet Ana Roxanne and dembow mutator DJ Python, double down on their acclaimed Times Square DJ set with a crystalline definition of trip-gaze subtractions like a whimsical fusion of Julee Cruise, Boards of Canada,  Seefeel and Cucina Povera.',
+		rating: 4,
+		// url: ,
+		explore: false,
+		'top-sales': false,
+		recommended: true
+	},
+	{
+		artist: 'Spivak',
+		title: 'You Win Again',
+		'release-date': '14-07-2023',
+		'record-label': ' Ecstatic',
+		'wholesale price': 12,
+		description:
+			'Bursting with bittersweet, memorable songs, Maria Spivak follows her spectacular and highly sought-after debut album with this new LP for Ecstatic, operating at the intersection of blazed synthpop, folk and shoegaze with a sparkling Drexciyan dimension. Real strong gear that comes highly recommended if yr into anything from Cocteau Twins to Junior Boys, Jonnine to Jessy Lanza.',
+		rating: 4,
+		// url: ,
+		explore: false,
+		'top-sales': false,
+		recommended: true
+	}
+];
