afa2982b88957047b6aa639e80a402c551f35618
Katrien S <ontwerp@katriens.be>
2023-12-06 18:10:33 +0100

    6 - setup html/css

---
+++ 100-iCodeMas-2023/assets/js/data06.js
@@ -0,0 +1,12 @@
+const products = [
+	{
+		id: 1,
+		name: 'Christmas Tree Decoration',
+		img: '../images/tree.png',
+		price: 19.99,
+		discount: 0,
+		cat: 1,
+		featured: true,
+		recent: true
+	}
+];
