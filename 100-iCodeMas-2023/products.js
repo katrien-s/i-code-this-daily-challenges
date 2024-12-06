142ccc82a9176976f73f1b26588d0013b5cce3a9
Katrien S <ontwerp@katriens.be>
2024-12-06 12:52:32 +0100

    Adding older solution day 6

--- 100-iCodeMas-2023/products.js
+++ 100-iCodeMas-2023/products.js
@@ -43,5 +43,104 @@ export const products = [
 		size: 'small',
 		new: false,
 		featured: true
+	},
+	{
+		id: 4,
+		name: 'snowflake decoration',
+		img: '../images/snowflake.png',
+		price: 4.99,
+		discount: 0,
+		category: 'ornaments',
+		color: 'white',
+		size: 'medium',
+		new: false
+	},
+	{
+		id: 5,
+		name: 'Star-shaped Cookie',
+		img: '../images/star_cookie.png',
+		price: 12.99,
+		discount: 0,
+		category: 'cookies',
+		color: 'brown',
+		size: 'small',
+		new: false
+	},
+	{
+		id: 6,
+		name: "Santa's hat",
+		img: '../images/santa_hat.png',
+		price: 39.99,
+		discount: 0,
+		category: 'accessories',
+		color: 'red',
+		size: 'large',
+		new: false
+	},
+	{
+		id: 7,
+		name: 'Candy Cane',
+		img: '../images/candy_cane.png',
+		price: 19.99,
+		discount: 0,
+		category: 'candies',
+		color: 'red',
+		size: 'medium',
+		new: false
+	},
+	{
+		id: 8,
+		name: 'snowman decoration',
+		img: '../images/snowman_deco.png',
+		price: 39.99,
+		discount: 0,
+		category: 'ornaments',
+		color: 'white',
+		size: 'small',
+		new: false
+	},
+	{
+		id: 9,
+		name: 'christmas ball',
+		img: '../images/christmas_ball.png',
+		price: 4.99,
+		discount: 0,
+		category: 'ornaments',
+		color: 'red',
+		size: 'small',
+		new: false
+	},
+	{
+		id: 10,
+		name: 'christmas red socks',
+		img: '../images/christmas_sock.png',
+		price: 29.99,
+		discount: 0,
+		category: 'accessories',
+		color: 'red',
+		size: 'small',
+		new: false
+	},
+	{
+		id: 11,
+		name: 'red ribbon',
+		img: '../images/red_ribbon.png',
+		price: 3.99,
+		discount: 0,
+		category: 'accessories',
+		color: 'red',
+		size: 'medium',
+		new: false
+	},
+	{
+		id: 12,
+		name: 'christmas tree cookie',
+		img: '../images/tree_cookie.png',
+		price: 13.99,
+		discount: 0,
+		category: 'cookies',
+		color: 'brown',
+		size: 'small',
+		new: false
 	}
 ];
