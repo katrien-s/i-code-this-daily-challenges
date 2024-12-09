bdc4a1c606b01e1f245609900fa72edf96b25cc7
Katrien S <ontwerp@katriens.be>
2024-12-09 17:09:47 +0100

    Adding older solution day 9

--- 100-iCodeMas-2023/app.js
+++ 100-iCodeMas-2023/app.js
@@ -34,3 +34,36 @@ function updateCartCount(count) {
 }
 
 updateCartCount(2);
+
+// Filter Selection Custom Dropdown
+
+const dropdownMenu = document.getElementById('custom-dropdown');
+const selectedOption = document.querySelector('.selected-option');
+const dropdownOptions = document.querySelector('.dropdown-options');
+const dropdownOption = document.querySelectorAll('.dropdown-option');
+
+dropdownMenu.addEventListener('click', toggleDropdown);
+
+function toggleDropdown() {
+	dropdownOptions.style.display =
+		dropdownOptions.style.display === 'flex' ? 'none' : 'flex';
+}
+
+function selectOption(option) {
+	var selectedText = document.querySelector('.selected-text');
+	selectedText.textContent = option;
+	toggleDropdown();
+}
+
+dropdownOption.forEach((option) => {
+	option.addEventListener('click', function () {
+		selectOption(option.textContent);
+	});
+});
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+	if (event.target !== dropdownMenu && !dropdownMenu.contains(event.target)) {
+		dropdownOptions.style.display = 'none';
+	}
+};
