72cbb2cb4b344d1c668d36a4f55f4d7137e66749
Katrien S <ontwerp@katriens.be>
2024-12-21 11:36:54 +0100

    Update solution day 21

--- 100-iCodeMas-2023/app.js
+++ 100-iCodeMas-2023/app.js
@@ -152,3 +152,18 @@ function createPagination(totalPages, page) {
 	paginationEl.innerHTML = paginationLi;
 	return paginationLi;
 }
+
+// FAQ
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+	const btn = question.querySelector('.question-btn');
+	btn.addEventListener('click', () => {
+		questions.forEach((item) => {
+			if (item !== question) {
+				item.classList.remove('show-text');
+			}
+		});
+		question.classList.toggle('show-text');
+	});
+});
